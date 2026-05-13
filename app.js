ee9d09cac75f97e95956f54efc513790c04052bc
G-@ndi <113950356+Giomar-Andi@users.noreply.github.com>
2026-05-12 20:59:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -277,8 +277,8 @@ function confirmDelete(storageKey, id) {
         cancelButtonText: 'Cancelar',
         
         // AQUÍ LA MAGIA DEL FONDO:
-        //backgroundImage: 'url("bg-sweet.jpg")', // O usa 'url("bg-sweet.jpg")' si configuraste la clase CSS
-        //color: '#fff',
+        backgroundImage: 'url("bg-sweet.jpg")', // O usa 'url("bg-sweet.jpg")' si configuraste la clase CSS
+        color: '#fff',
         
         // Si quieres usar la imagen de fondo completa, descomenta la siguiente línea:
         customClass: { popup: 'kuromi-bg-alert' } 
