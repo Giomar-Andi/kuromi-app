75c4babbfd76762b76411c13dcf9eef3f54414ff
G-@ndi <113950356+Giomar-Andi@users.noreply.github.com>
2026-05-12 20:50:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -277,7 +277,7 @@ function confirmDelete(storageKey, id) {
         cancelButtonText: 'Cancelar',
         
         // AQUÍ LA MAGIA DEL FONDO:
-        background: 'rgba(46, 0, 79, 0.95)', // O usa 'url("bg-sweet.jpg")' si configuraste la clase CSS
+        background: 'url("bg-sweet.jpg")', // O usa 'url("bg-sweet.jpg")' si configuraste la clase CSS
         color: '#fff',
         
         // Si quieres usar la imagen de fondo completa, descomenta la siguiente línea:
